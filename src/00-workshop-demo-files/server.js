8c415fac19c353d4383f4c9f11068f869276e162
Dani Lucaci <danilucaci@gmail.com>
2021-01-12 10:00:00 +0100

    feat: add dev:workshop solution

--- src/00-workshop-demo-files/server.js
+++ src/00-workshop-demo-files/server.js
@@ -2,15 +2,22 @@ const express = require("express");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const { json } = require("body-parser");
+const cors = require("cors");
 
-const UserRouter = require("./routes/user-routes");
+const userRouter = require("./routes/user-routes");
+const config = require("./config/config");
 
 const app = express();
 
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(json());
+app.use(
+  cors({
+    origin: config.client.URL,
+  }),
+);
 
-app.use("/users", UserRouter);
+app.use(userRouter);
 
 module.exports = app;
