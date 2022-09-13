94794713583f9cf092e4cb21c84a33bcc49f163c
Ivan Escribano <ivanescribano1998@gmail.com>
2022-09-13 18:16:00 +0200

    finished pill routing with express

--- src/controllers/book-controller.js
+++ src/controllers/book-controller.js
@@ -35,7 +35,23 @@ const { logger } = require("../config/config");
  * Wrap the code in a try/catch statement and call next(error)
  * with the error object that is caught
  */
-async function createBook() {}
+async function createBook(req, res, next) {
+  const { title, author, genre, year, pages } = req.body;
+  try {
+    const book = await db.Book.create({
+      title,
+      author,
+      genre,
+      year,
+      pages,
+    });
+    res.status(201).send({
+      data: book._id,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
 
 /**
  * 1. Create the book CRUD controllers
@@ -54,7 +70,19 @@ async function createBook() {}
  *
  * And call lean() and exec() on the query
  */
-async function getBooks() {}
+async function getBooks(req, res, next) {
+  try {
+    const book = await db.Book.find({})
+      .select({ _id: 1, title: 1 })
+      .lean()
+      .exec();
+    res.status(200).send({
+      data: book,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
 
 /**
  * 1. Create the book CRUD controllers
@@ -88,7 +116,23 @@ async function getBooks() {}
  *
  * And call lean() and exec() on the query
  */
-async function getSingleBook() {}
+async function getSingleBook(req, res, next) {
+  try {
+    const book = await db.Book.find({ _id: req.params.bookId })
+      .select({
+        title: 1,
+        pages: 1,
+      })
+      .populate("author", { _id: 1, firstName: 1, lastName: 1 })
+      .lean()
+      .exec();
+    res.status(200).send({
+      data: book,
+    });
+  } catch (error) {
+    next(error);
+  }
+}
 
 /**
  * 1. Create the book CRUD controllers
@@ -115,7 +159,19 @@ async function getSingleBook() {}
  * Wrap the code in a try/catch statement and call next(error)
  * with the error object that is caught
  */
-async function updateBook() {}
+async function updateBook(req, res, next) {
+  try {
+    const { bookId } = req.params;
+    const doc = await db.Book.findOneAndUpdate(
+      { _id: bookId },
+      { title: "TITLE UPDATES", pages: 333 },
+      { new: true },
+    );
+    res.status(200).send({ data: { doc } });
+  } catch (error) {
+    next(error);
+  }
+}
 
 /**
  * 1. Create the book CRUD controllers
@@ -135,7 +191,15 @@ async function updateBook() {}
  * Wrap the code in a try/catch statement and call next(error)
  * with the error object that is caught
  */
-async function deleteBook() {}
+async function deleteBook(req, res, next) {
+  try {
+    const { bookId } = req.params;
+    const doc = await db.Book.findOneAndDelete({
+      _id: bookId,
+    });
+    res.status(200).send({ data: { _id: doc._id } });
+  } catch (error) {}
+}
 
 module.exports = {
   createBook: createBook,
--- src/db/connect.js
+++ src/db/connect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 function connect() {
-  return mongoose.connect("mongodb://localhost:27017/myApp", {
+  return mongoose.connect("mongodb://localhost:27017/mongo-pill-basics", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
